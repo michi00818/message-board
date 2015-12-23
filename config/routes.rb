e4d1633cc3e916f2b87403906abc1fe56616aad3
michi00818 <michi00818@gmail.com>
2015-12-23 06:54:35 +0000

    render form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'messages#index'
-  resources :messages , only: [:create]
+  resources :messages , except: [:index,:new]
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
-  # Example resource route with options:
+# Example resource route with options:
   #   resources :products do
   #     member do
   #       get 'short'
